d42653460f5c254a2dfc35bb6f8000d3dc646813
Thomas Huynh <thuynh@axome.com>
2022-05-26 19:35:15 +0200

    gazetto update

--- commands/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'test',
-
-    async execute (client, message, args, Discord) {
-        message.reply('Je suis vivant')
-    }
-}
--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-const Discord = require(`discord.js`);
-const client = new Discord.Client();
-const Canvas = require('canvas');
 require('dotenv').config()
+const { Client, Intents } = require('discord.js');
 
-client.commands = new Discord.Collection();
-client.events = new Discord.Collection();
-['command_handler', 'event_handler'].forEach(handler =>{
-    require (`./handlers/${handler}`)(client, Discord);
-})
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 
-//Welcome message
-var welcomeCanvas  = {};
-welcomeCanvas.create = Canvas.createCanvas(1024, 370)
-welcomeCanvas.context = welcomeCanvas.create.getContext('2d')
-welcomeCanvas.context.font = '20px sans-serif';
-welcomeCanvas.context.fillStyle = '#ffffff';
+client.once('ready', () => {
+    console.log('Gazette on !');
+});
 
-Canvas.loadImage("./img/bg.png").then(async (img) => {
-    welcomeCanvas.context.drawImage(img, 0, 0, 1024, 370)
-    welcomeCanvas.context.beginPath();
-    welcomeCanvas.context.arc(175, 166, 128, 0, Math.PI * 2, true);
-    welcomeCanvas.context.stroke();
-    welcomeCanvas.context.fill();
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
 
-})
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('id de l\'avatar : ' + interaction.user.avatar + '\nNom du channel : ' + interaction.channel.name)
+    } else if (commandName === 'server') {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    } else if (commandName === 'user') {
+        await interaction.reply('User info.');
+    }
+});
+
+client.login(process.env.TOKEN);
+
+// const Canvas = require('canvas');
+//
+// //Welcome message
+// var welcomeCanvas  = {};
+// welcomeCanvas.create = Canvas.createCanvas(1024, 370)
+// welcomeCanvas.context = welcomeCanvas.create.getContext('2d')
+// welcomeCanvas.context.font = '20px sans-serif';
+// welcomeCanvas.context.fillStyle = '#ffffff';
+
+// Canvas.loadImage("./img/bg.png").then(async (img) => {
+//     welcomeCanvas.context.drawImage(img, 0, 0, 1024, 370)
+//     welcomeCanvas.context.beginPath();
+//     welcomeCanvas.context.arc(175, 166, 128, 0, Math.PI * 2, true);
+//     welcomeCanvas.context.stroke();
+//     welcomeCanvas.context.fill();
+//
+// })
 
 // client.on('guildMemberAdd', async member => {
 //     const welcomeChannel = client.channels.cache.get('756778491940175876')
@@ -49,6 +65,3 @@ Canvas.loadImage("./img/bg.png").then(async (img) => {
 //     }
 // })
 
-
-//Token
-client.login(process.env.TOKEN);
